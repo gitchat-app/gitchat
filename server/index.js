fb937f438e5c34e3bf8c841e4b1eb0e4224a1df9
Eric Vere <erkver250@gmail.com>
2018-11-09 12:23:43 -0600

    Set up basic server, routes, and views

---
+++ server/index.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+
+const { SECRET, CONNECTION_STRING } = process.env;
+const express = require("express");
+const session = require("express-session");
+const app = express();
+const port = process.env.PORT || 3001;
+const { json } = require("body-parser");
+
+app.use(json());
+
+app.use(
+  session({
+    secret: SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 10000000000
+    }
+  })
+);
+
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
