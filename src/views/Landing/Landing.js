9f468901d394ba7cb3e8fa24d8107cf84203a5c1
Ray Olivier <rayolivier@outlook.com>
2018-11-12 09:46:31 -0600

    more setup

---
+++ src/views/Landing/Landing.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./Landing.scss";
+
+import firebase from "../../firebase";
+
+class Landing extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Landing </h1>
+      </div>
+    );
+  }
+}
+
+export default Landing;
