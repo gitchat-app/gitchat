d7c68af4e6d05f2aea3c46fcd626aa23c2543be6
John MacIsaac <johnmi@protonmail.com>
2018-11-16 13:42:57 -0600

    updated profile

--- src/views/Profile/Profile.js
+++ src/views/Profile/Profile.js
@@ -7,6 +7,7 @@ class Profile extends Component {
     super();
     this.state = {
       user: {},
+      uid: "",
       avatar: "",
       username: "",
       name: "",
@@ -19,11 +20,11 @@ class Profile extends Component {
     firebase.auth().onAuthStateChanged(
       function(user) {
         if (user) {
-          console.log("user:", user);
           const ref = firebase.database().ref(`users/${user.uid}`);
-          ref.on("value", (snapshot) => {
+          ref.on("value", snapshot => {
             console.log("snapshot:", snapshot.val());
             this.setState({ user: snapshot.val() });
+            this.setState({ uid: snapshot.val().uid });
             this.setState({ avatar: snapshot.val().avatar });
             this.setState({ username: snapshot.val().username });
             this.setState({ name: snapshot.val().name });
@@ -38,20 +39,33 @@ class Profile extends Component {
   }
 
   render() {
-    // const uid = this.state.user.uid;
-    // console.log('uid: ', uid);
-
+    const uid = firebase.database();
+    console.log(uid)
+    
     return (
       <div className="profile">
         <h1>Profile</h1>
         <br />
         {this.state.edit === false ? (
           <>
+            <div>
+              {/* <img src={this.state.user.avatar} alt="avatar_image" /> */}
+              <img
+                className="form-img"
+                src={this.state.avatar}
+                alt=""
+                onError={
+                  e =>
+                    (e.target.src =
+                      "https://www.biber.com/dta/themes/biber/core/assets/images/no-featured-175.jpg") // when this.state.imgurl doesn't lead to an img, use the url above
+                }
+              />
+            </div>
             <br />
-            {/* <img src={this.state.user.avatar} alt="avatar_image" /> */}
             <h3>{this.state.user.username}</h3>
             <br />
             <h3> {this.state.user.name} </h3>
+            <br />
             <h3>{this.state.user.email}</h3>
             <br />
             <button onClick={() => this.setState({ edit: true })}>
@@ -64,34 +78,37 @@ class Profile extends Component {
             <input
               value={this.state.avatar}
               placeholder="Avatar"
-              onChange={(e) => this.setState({ avatar: e.target.value })}
+              onChange={e => this.setState({ avatar: e.target.value })}
             />
-            {/* USER ENTERED URL input: 
+
+            {/* 
+            SAMPLE USER ENTERED URL: 
             https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50 
+            DEFAULT: http://laurauinteriordesign.com/wp-content/uploads/2018/03/avatar-placeholder.png
             */}
 
             <h3>username</h3>
             <input
               value={this.state.username}
               placeholder="Username"
-              onChange={(e) => this.setState({ username: e.target.value })}
+              onChange={e => this.setState({ username: e.target.value })}
             />
 
             <h3>name</h3>
             <input
               value={this.state.name}
               placeholder="Name"
-              onChange={(e) => this.setState({ name: e.target.value })}
+              onChange={e => this.setState({ name: e.target.value })}
             />
 
             <h3>email</h3>
             <input
               value={this.state.email}
               placeholder="email"
-              onChange={(e) => this.setState({ email: e.target.value })}
+              onChange={e => this.setState({ email: e.target.value })}
             />
-
-            <button onClick={(e) => this.setState({ edit: false })}>
+            <br />
+            <button onClick={e => this.setState({ edit: false })}>
               Cancel
             </button>
             <div>
@@ -99,24 +116,39 @@ class Profile extends Component {
                 onClick={() => {
                   firebase
                     .database()
-                    .ref("users/" + this.state.user.uid)
+                    .ref("users/" + this.state.uid)
                     .update({
                       avatar: this.state.avatar,
                       username: this.state.username,
                       name: this.state.name,
                       email: this.state.email
                     });
+                  this.setState({
+                    edit: false
+                  });
                 }}
               >
                 Save Changes
               </button>
             </div>
-
-            <div>
-              {/* <button onClick={() => userId.delete()}>
+            <br />
+            {/* <div>
+              <button
+                onClick={() =>
+                  // clicking 'OK' returns true, 'Cancel' false
+                  window.confirm("Confirm: Yes, delete my account")
+                    // ? this.state.uid.remove()
+                    ? 
+                    (
+                      firebase.database().ref(`users/${this.state.uid}`).remove();
+                    // need to exit to home page
+                    )
+                    : null
+                }
+              >
                 Delete Account
-              </button> */}
-            </div>
+              </button>
+            </div> */}
           </>
         )}
       </div>
