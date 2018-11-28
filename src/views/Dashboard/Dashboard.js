caefb5c8a568878ad5d2bfc7c2f47cd901337d9b
Ray Olivier <rayolivier@outlook.com>
2018-11-28 09:08:05 -0600

    find servers function

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -13,7 +13,9 @@ class Dashboard extends Component {
       userServers: [],
       servers: {},
       friends: {}
-    }
+    };
+
+    this.findServers = this.findServers.bind(this);
   }
   componentDidMount() {
     const { user, servers, userServers } = this.state;
@@ -34,31 +36,31 @@ class Dashboard extends Component {
         });
       }
     });
-    friendsRef.on("value", snapshot => {
+    friendsRef.on("value", (snapshot) => {
       // console.log("snapshot:", snapshot.val());
       this.setState({ friends: snapshot.val() });
     });
-    serverRef.on("value", snapshot => {
+    serverRef.on("value", (snapshot) => {
       console.log("snapshot:", snapshot.val());
       // let allServers = [];
       // let serverCopy = servers.slice();
       // snapshot.forEach(childSnap => {
-        // allServers.push(childSnap.key);
-        // let key = childSnap.key;
-        // childSnap.
-        // childSnap.child("members").forEach(member => {
-        //   console.log(member.key, user.uid);
-        //   if(member.key !== user) {
-        //     serverCopy.push(key);
-        //   }
-        // });
-        // allServers.filter(userKey => {
-        //   // userKey !== key ? serverCopy.push(key) : null
-        //   console.log(userKey, key);
-        //   return serverCopy;
-        // });
-        // console.log(childSnap.child(`members`).val());
-        // if(childSnap.val())
+      // allServers.push(childSnap.key);
+      // let key = childSnap.key;
+      // childSnap.
+      // childSnap.child("members").forEach(member => {
+      //   console.log(member.key, user.uid);
+      //   if(member.key !== user) {
+      //     serverCopy.push(key);
+      //   }
+      // });
+      // allServers.filter(userKey => {
+      //   // userKey !== key ? serverCopy.push(key) : null
+      //   console.log(userKey, key);
+      //   return serverCopy;
+      // });
+      // console.log(childSnap.child(`members`).val());
+      // if(childSnap.val())
       // });
       // console.log(allServers);
       // allServers.map(server => {
@@ -66,20 +68,67 @@ class Dashboard extends Component {
       //   })
       //   !== server ? serverCopy.push(server) : null));
       this.setState({ servers: snapshot.val() });
-    });  
+    });
+
+    if (this.state.user.uid) {
+      this.findServers();
+    }
+  }
+
+  findServers() {
+    //NOTE: if you just call findServers() in the render, it will give an infinite loop error. if it's in the componentDidMount, it will get called before this.state.user.uid exists. (you can probably do it in the mount if you do an async await for the user info to come back)
+    let singleServerList = [];
+
+    //this console.log is to show each time the loop is run
+    console.log("FINDING SERVERS");
+    for (let key in this.state.servers) {
+      let singleServer = (
+        <div className="dash-server-card" key={key}>
+          <SingleServer objKey={key} />
+        </div>
+      );
+
+      if (this.state.user.uid) {
+        const serverRef = firebase
+          .database()
+          .ref(`servers/${key}/members/${this.state.user.uid}`);
+        serverRef.once("value", (snap) => {
+          //if the snap exists, then the user is in that server, so we want to push the ones the user ISN'T in via the else
+          if (snap.exists()) {
+            console.log("EXISTS", key, snap.val());
+          } else {
+            console.log("DOESN'T EXIST", key);
+            singleServerList.push(singleServer);
+          }
+        });
+      } else {
+        console.log("No user.uid");
+      }
+    }
+    console.log("singleServerList", singleServerList);
+    //singleServerList is now a list of servers the user isn't in
+
+    // this.setState({ singleServerList });
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props !== prevProps) {
+      console.log("UPDATED");
+    }
   }
 
   render() {
+    // if (this.state.user.uid) {
+    //   this.findServers();
+    // }
     const { userServers, servers, friends, user } = this.state;
-    console.log(this.state);
+    // console.log(this.state);
     let onlineList = [];
     let offlineList = [];
     let dashList = [];
     let colorStatus = "";
     for (let key in friends) {
-      let loggedOff = moment
-        .unix(friends[key].ended / 1000)
-        .fromNow();
+      let loggedOff = moment.unix(friends[key].ended / 1000).fromNow();
       // console.log(moment.unix(users[key].ended / 1000).fromNow());
       if (friends[key].status === "online") {
         colorStatus = "#e0e0e0";
@@ -89,15 +138,25 @@ class Dashboard extends Component {
         colorStatus = "#5a7164";
       }
       // console.log(users[key].status);
-      let singleUser = <div className="userList-cont" key={key}>
-        <div style={{"background": colorStatus}} className="online-status-color"></div>
-        <p>{friends[key].username}</p>
-        <img src={friends[key].avatar} alt="" />
-        </div>;
+      let singleUser = (
+        <div className="userList-cont" key={key}>
+          <div
+            style={{ background: colorStatus }}
+            className="online-status-color"
+          />
+          <p>{friends[key].username}</p>
+          <img src={friends[key].avatar} alt="" />
+        </div>
+      );
       if (friends[key].status === "online") {
         onlineList.push(singleUser);
       } else if (friends[key].status === "offline") {
-        offlineList.push(<>{singleUser}<h5>Logged off {loggedOff}...</h5></>);
+        offlineList.push(
+          <>
+            {singleUser}
+            <h5>Logged off {loggedOff}...</h5>
+          </>
+        );
       }
     }
     for (let key in servers) {
@@ -106,16 +165,18 @@ class Dashboard extends Component {
           <SingleServer objKey={key} />
         </div>
       );
-      console.log(user);
+      // console.log(user);
       // console.log(Object.keys(servers[key].members));
       // console.log('servers[key].members: ', servers[key].members);
       let keys = Object.keys(servers[key].members);
       // dashList.push(singleServer);
-      keys.map(key => key !== user.uid ? dashList.push(singleServer) : null);
+      keys.map((key) =>
+        key !== user.uid ? dashList.push(singleServer) : null
+      );
       // for(let member in servers[key].members) {
       //   console.log(member);
       //   if(member === user.uid) {
-          
+
       //   }
       // }
       // userServers.map(userKey => {
@@ -139,6 +200,7 @@ class Dashboard extends Component {
           <h1>Dashboard</h1>
           <div className="scrollbar" id="style-8">
             {dashList}
+            {this.state.singleServerList}
           </div>
         </div>
         <div className="friends-cont">
