da721b5157b1c53aee24300bea54b89108ab31dd
John MacIsaac <johnmi@protonmail.com>
2018-11-29 15:51:25 -0600

    added features to homepage etc

--- src/views/Landing/Landing.js
+++ src/views/Landing/Landing.js
@@ -1,22 +1,76 @@
-
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./Landing.scss";
-import Login from "../Login/Login";
 import firebase from "../../firebase";
 import logo from "../../media/logo_transparent.png";
 
-function Home(props) {
-  return (
-    <div className="home-page">
-      <span className="home-page-links">
-        <Link to="/server/-LRJIG0Y_f2mtEVOL5CE" className="home-page-link">View Global Server </Link>
-        <Link to="/login" className="home-page-link"> Login / Signup</Link>
-      </span>
-      <span className="logo-container">
-        <img className="logo_transparent" alt="logo&tagline" src={logo} />
-      </span>
-    </div>
-  );
+class Landing extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedIn: null
+    };
+  }
+
+  componentDidMount() {
+    // if !logged in, return login link, else logout link
+    firebase.auth().onAuthStateChanged(user => {
+      if (!user) {
+        this.setState({ loggedIn: false });
+      } else {
+        this.setState({ loggedIn: true });
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="homepage">
+        <div className="homepage-links">
+          <Link to="/server/-LRJIG0Y_f2mtEVOL5CE" className="homepage-link">
+            View Global Server{" "}
+          </Link>
+          {!this.state.loggedIn ? (
+            <Link to="/login" className="homepage-link">
+              {" "}
+              Login / Signup
+            </Link>
+          ) : (
+            <button
+              className="homepage-link homepage-signout-button"
+              onClick={() => firebase.auth().signOut()}
+            >
+              Log out
+            </button>
+          )}
+        </div>
+        <div className="logo-container">
+          <img className="logo_transparent" alt="logo&tagline" src={logo} />
+        </div>
+
+        <div className="homepage-footer">
+          <div className="homepage-footer-group">
+            {" "}
+            <h4>Direct Messaging</h4>{" "}
+            <p>Use direct messaging to communicate with individuals.</p>
+          </div>
+
+          <div className="homepage-footer-group">
+            {" "}
+            <h4>Servers</h4>{" "}
+            <p>Create or join server channels to communicate as a group. </p>
+            <p>Upload code snippets (which preserve code indentation). </p>
+          </div>
+
+          {/* <div className="homepage-footer-group">
+            {" "}
+            <h4>About</h4>{" "}
+            <p> </p>
+          </div> */}
+        </div>
+      </div>
+    );
+  }
 }
-export default Home;
+
+export default Landing;
